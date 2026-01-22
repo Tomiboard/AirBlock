0355ad1d238f0c48a020592622f52e20b55a1b76
thomas <thomasmujika1@gmail.com>
2026-01-23 00:24:00 +0100

    Initial commit.

---
+++ app/src/main/java/com/example/airblock/AirBlockHome.kt
@@ -0,0 +1,90 @@
+package com.example.airblock
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.airblock.ui.theme.AirBlockTheme
+
+
+@Composable
+fun AirBlockHomeScreen() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.dark_background))
+    ) {
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 100.dp)
+        ) {
+
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(
+                        id = R.color.button_surface
+                    )
+                ),
+                modifier = Modifier
+                    .padding(bottom = 30.dp)
+                    .width(250.dp)
+                    .height(50.dp)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = if (!AirBlockState.hasTagRegistered) stringResource(id = R.string.bts_scan)
+                    else stringResource(id = R.string.btn_manage)
+                )
+            }
+
+            Text(
+                text = stringResource(id = R.string.buy_tag),
+                color = Color.White,
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .padding(bottom = 40.dp)
+                    .align(Alignment.CenterHorizontally)
+
+            )
+
+            Text(
+                text = stringResource(id = R.string.any_tag_info),
+                color = Color.Gray,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+
+
+        }
+
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AirBlockHome() {
+    AirBlockTheme {
+        AirBlockHomeScreen()
+    }
+}
---
+++ app/src/main/java/com/example/airblock/AirBlockState.kt
@@ -0,0 +1,10 @@
+package com.example.airblock
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+object AirBlockState   {
+    var hasTagRegistered by mutableStateOf(false)
+    var isLocked by mutableStateOf(false)
+}
